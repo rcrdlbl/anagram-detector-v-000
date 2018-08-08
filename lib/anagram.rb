140352d2b79d5c09c33a3690234979ee6adb734f
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-08 23:31:49 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(matches)
+    split_word = @word.split("")
+    matches.select {|match| match.split("").sort == split_word.sort}
+  end
+
+end
